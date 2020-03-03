e54a6ca01fcae73987e302d38fd19a367b948206
max denton <max.t.denton@gmail.com>
2020-03-03 13:37:16 +0000

    pushing just the fixes to some of the problems

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,22 +1,31 @@
 class UsersController < ApplicationController
+
+  def show
+    @user = User.find(params[:id])
+  end
+
   def new
     @user = User.new
   end
 
   def create
-    user = User.new(params_user)
-    user.save ? redirect_to root : render :new
+    @user = User.new(params_user)
+    if @user.save
+      redirect_to root
+    else
+      render :new
+    end
   end
 
   def edit
-    
+
   end
 
   def update
-    
+
   end
 
   def delete
-    
+
   end
 end
