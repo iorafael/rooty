1758232e1a7133cbc6fda9c9d1bc3bdc6e903877
max denton <max.t.denton@gmail.com>
2020-03-03 11:25:06 +0000

    added the animation of the navbar

--- db/schema.rb
+++ db/schema.rb
@@ -8,11 +8,9 @@
 # migrations from scratch. Old migrations may fail to apply correctly if those
 # migrations use external dependencies or application code.
 #
-# It's strongly recommended that you check this file into your version control sys
-ActiveRecord::Schema.define(version: 2020_03_02_142205) do
+# It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 2020_03_02_143012) do
-
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
