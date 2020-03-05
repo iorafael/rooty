ca8d1674b7e386ebd227ef952355ae23d7814a72
max denton <max.t.denton@gmail.com>
2020-03-05 10:35:07 +0000

    added me as a user in seedfile

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,33 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+user1=User.create!(name:'max', lastname:"denton", email:"max@mail.com", username:"bakemypotatoes", password:"123456")
+
+Route.create!(user: user1)
+Node.create!(name: "Max's house", route: Route.second, real:true, address: "SW18 1PS")
+Node.create!(name: "T1.1", route: Route.first, real: false)
+Node.create!(name: "T1.2", route: Route.first, real: false)
+Node.create!(name: "T1.3", route: Route.first, real: false)
+
+
+Route.create!(user: user1)
+Node.create!(name: "stinky's house", route: Route.second, real:true, address: "SW13 0LW")
+Node.create!(name: "T1.1", route: Route.first, real: false)
+Node.create!(name: "T1.2", route: Route.first, real: false)
+Node.create!(name: "T1.3", route: Route.first, real: false)
+
+Route.create!(user: user1)
+Node.create!(name: "willies house", route: Route.second, real:true, address: "NW2 2AR")
+Node.create!(name: "T1.1", route: Route.first, real: false)
+Node.create!(name: "T1.2", route: Route.first, real: false)
+Node.create!(name: "T1.3", route: Route.first, real: false)
+
+Route.create!(user: user1)
+Node.create!(name: "Max's house", route: Route.second, real:true, address: "SW15 2SP")
+Node.create!(name: "T1.1", route: Route.first, real: false)
+Node.create!(name: "T1.2", route: Route.first, real: false)
+Node.create!(name: "T1.3", route: Route.first, real: false)
+
+Route.create!(user: user1)
+Node.create!(name: "Max's house", route: Route.second, real:true, address: "KT12 3PU")
+Node.create!(name: "T1.1", route: Route.first, real: false)
+Node.create!(name: "T1.2", route: Route.first, real: false)
+Node.create!(name: "T1.3", route: Route.first, real: false)
+
