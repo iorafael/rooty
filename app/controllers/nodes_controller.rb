e206f1401f36256de0574a52941838789208a46a
Stephano Wurttele <stephano.wurttele@utec.edu.pe>
2020-03-05 19:56:06 +0000

    Fixed address not found

--- app/controllers/nodes_controller.rb
+++ app/controllers/nodes_controller.rb
@@ -3,12 +3,19 @@ class NodesController < ApplicationController
         distance = params[:node][:distance] ? params[:node][:distance] : 5
         newroute = Route.create!(user: current_user)
         init = Node.create!(name: "Max's house", route: newroute, real:true, address: params[:node][:address])
-        long = init.longitude
-        lat = init.latitude
-        Node.create!(name: "T1.1", route: newroute, real: false, longitude: long + 0.0012 * distance, latitude: lat)
-        Node.create!(name: "T1.2", route: newroute, real: false, longitude: long + 0.0012 * distance, latitude: lat + 0.002 * distance)
-        Node.create!(name: "T1.3", route: newroute, real: false, longitude: long, latitude: lat + 0.002 * distance)
-        redirect_to root_path(route: newroute.id)
+        if init.latitude    
+            long = init.longitude
+            lat = init.latitude
+            Node.create!(name: "T1.1", route: newroute, real: false, longitude: long + 0.0012 * distance, latitude: lat)
+            Node.create!(name: "T1.2", route: newroute, real: false, longitude: long + 0.0012 * distance, latitude: lat + 0.002 * distance)
+            Node.create!(name: "T1.3", route: newroute, real: false, longitude: long, latitude: lat + 0.002 * distance)
+            redirect_to root_path(route: newroute.id)
+        else
+            Node.last.delete
+            Route.last.delete
+            flash[:notice] = 'Address not found, please be more specific!'
+            redirect_to root_path
+        end
     end
 
 end
