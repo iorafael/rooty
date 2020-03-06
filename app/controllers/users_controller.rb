7ac45a8bd68cdc2ced89cf6a42add91203024d04
Stephano Wurttele <stephano.wurttele@utec.edu.pe>
2020-03-06 16:10:14 +0000

    Done

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,9 +2,8 @@ class UsersController < ApplicationController
 
   def show
     participants = Participant.where(user: current_user)
-    @participants = participants.map { |participant| Route.find(participant.id) }.uniq
+    @events = participants.map { |participant| Event.find(participant.id) }.uniq
     @routes = current_user.routes
-    @events = Event.all
   end
 
   def edit
