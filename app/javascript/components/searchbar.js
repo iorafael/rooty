61d9cffedd5c1518db15b5e917d25d872061a06f
Stephano Wurttele <stephano.wurttele@utec.edu.pe>
2020-03-06 14:35:54 +0000

    Fix javascript

--- app/javascript/components/searchbar.js
+++ app/javascript/components/searchbar.js
@@ -1,3 +1,6 @@
-document.querySelector('.searchbar').addEventListener('touchstart',(event) => {
- event.currentTarget.classList.toggle('hover_effect')
-})
+searchbar = document.querySelector('.searchbar')
+    if(searchbar){
+        searchbar.addEventListener('touchstart',(event) => {
+    event.currentTarget.classList.toggle('hover_effect')
+    })
+}
