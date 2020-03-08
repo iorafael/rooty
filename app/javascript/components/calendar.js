0b81a12abbb816019e4e178407271e8b5da13ea7
Stephano Wurttele <stephano.wurttele@utec.edu.pe>
2020-03-08 01:36:57 +0000

    Removed console logs

--- app/javascript/components/calendar.js
+++ app/javascript/components/calendar.js
@@ -8,7 +8,6 @@ const events = document.querySelectorAll(".event-creator");
 if (events){
     events.forEach((event) => {
             event.addEventListener("click", (event) => {
-            console.log(event)
             const route = event.currentTarget.dataset.route;
             $('#event_route').val(route);
         });
