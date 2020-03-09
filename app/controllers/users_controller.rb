1b3ec9206d2df4ffcc8442ede4dce0e036209407
Rafael Santos <SANTTRAFAEL@GMAIL.COM>
2020-03-09 11:47:53 +0000

    photo on show event page and menu yellow

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
 
   def show
     participants = Participant.where(user: current_user)
-    @events = participants.map { |participant| Event.find(participant.id) }.uniq
+    @events = participants.map { |participant| Event.find(participant.event.id) }.uniq
     @routes = current_user.routes
   end
 
