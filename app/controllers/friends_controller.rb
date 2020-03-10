e95e1e5cac73c64e26c6b61a1736397546fc8991
Stephano Wurttele <stephano.wurttele@utec.edu.pe>
2020-03-10 20:12:53 +0000

    Add friend button working and creating. Friend REQUESTING missing

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -12,17 +12,19 @@ class FriendsController < ApplicationController
   end
 
   def create
-   @friend = Friend.new(user_id: current_user.id, user_friend_id: params[:id])
-   @user = User.find(params[:user_id])
-   if @friend.save
-    flash[:notice] = "Added friend."
-    respond_to do |format|
-      format.html { redirect_to @user }
-      format.js
-   else
-    flash[:error] = "Unable to add friend."
-    render 'users/show'
-   end
+    friend = Friend.new(user_id: current_user.id, user_friend_id: params[:user_id])
+    @user = User.find(params[:user_id])
+    if friend.save
+      flash[:notice] = "Added friend."
+      puts "A"
+      respond_to do |format|
+        format.html { redirect_to @user }
+        format.js
+      end
+    else
+      flash[:error] = "Unable to add friend."
+      render "users/#{params[:user_id]}"
+    end
   end
 
   def destroy
